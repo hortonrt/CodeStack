c7dab6e8a6dd083c59db2860773f78ce198923e5
Ryan Horton <ryan@codestackllc.com>
2018-08-06 15:53:03 -0400

    Working Environments

--- CodeStack.WebAPI/Startup.cs
+++ CodeStack.WebAPI/Startup.cs
@@ -1,4 +1,5 @@
-﻿using CodeStack.Data.Models;
+﻿using System;
+using CodeStack.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,34 +11,39 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CodeStack.WebAPI {
     public class Startup {
+        public IConfiguration Configuration { get; }
+        private IHostingEnvironment CurrentEnvironment { get; set; }
+        private bool isProd { get; set; }
+
         public Startup(IConfiguration configuration, IHostingEnvironment env) {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional : true, reloadOnChange : true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional : true, reloadOnChange : true);
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional : true, reloadOnChange : true)
+                .AddEnvironmentVariables();
 
+            if (env.IsDevelopment()) {
+                builder.AddUserSecrets<Startup>();
+            }
+            CurrentEnvironment = env;
             Configuration = builder.Build();
-            //Configuration = configuration;
+            isProd = Convert.ToBoolean(Configuration["IsProduction"]);
         }
-        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(options => {
                 options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
             });
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "dist"; });
-            services.AddDbContext<CodeStackContext>(options => options.UseSqlServer("Data Source=132.148.86.13;Initial Catalog=CodeStackLLC_Dev;uid=CodeStackLLC;password=vN99iwbv7!;MultipleActiveResultSets=true;"));
+            services.AddDbContext<CodeStackContext>(options => options.UseSqlServer(Configuration["connectionStrings:CodeStack" + (isProd ? "_Prod" : "_Dev")]));
             services.AddCors();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             } else {
-                app.UseExceptionHandler("/Error");
                 app.UseHsts();
                 app.UseHttpsRedirection();
             }
@@ -52,14 +58,10 @@ namespace CodeStack.WebAPI {
             });
 
             app.UseSpa(spa => {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
+                spa.Options.SourcePath = "dist";
                 if (env.IsDevelopment()) {
                     spa.Options.SourcePath = "../CodeStack.UI/dist";
                     spa.UseAngularCliServer(npmScript: "start");
-                } else {
-                    spa.Options.SourcePath = "dist";
                 }
             });
         }
