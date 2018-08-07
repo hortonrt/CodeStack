d6200ec00758271ea8b5a82ffb310c19f6f65ad9
Ryan Horton <ryan@codestackllc.com>
2018-08-06 22:55:42 -0400

    Main Page Finished

--- CodeStack.UI/src/app/pages/main/main.component.ts
+++ CodeStack.UI/src/app/pages/main/main.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  faGlobe,
+  faTerminal,
+  faDatabase,
+  faHome,
+  faCode,
+  faTabletAlt,
+  faDesktop,
+  faMobileAlt,
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  faGlobe = faGlobe;
+  faTerminal = faTerminal;
+  faDatabase = faDatabase;
+  faHome = faHome;
+  faCode = faCode;
+  faPhone = faMobileAlt;
+  faTablet = faTabletAlt;
+  faDesktop = faDesktop;
   constructor() {}
 
   ngOnInit() {}
